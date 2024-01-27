68c819be6c8941b0e70b0e58688d60b27ad0ae4b
morisum <moris@r.sannet.ne.jp>
2024-01-27 09:40:28 +0900

    Initial Commit

---
+++ EnvirnmentObjectSample2/EnvirnmentObjectSample2App.swift
@@ -0,0 +1,17 @@
+//
+//  EnvirnmentObjectSample2App.swift
+//  EnvirnmentObjectSample2
+//
+//  Created by MsMacM on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct EnvirnmentObjectSample2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
